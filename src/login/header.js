e4e0b714e9978c8fe6e6c711e5f8aca7c3e6658d
Jeff <rox.masterj@gmail.com>
2026-04-21 10:00:42 +0700

    add login

---
+++ src/login/header.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const header = () => {
+  return (
+    <View>
+      <Text>header</Text>
+    </View>
+  )
+}
+
+export default header
+
+const styles = StyleSheet.create({})
